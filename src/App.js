cfcc7c445a67a3415e501a4f27dc889e1d9e71fc
u101004132 <dzag17188@gmail.com>
2020-08-15 16:16:41 -0400

    turned function into class

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-      <div className="mapContainer">
-        ...put Map Component here...
+class App extends Component {
+  render() {
+    return (
+      <div className="app" >
+        <div className="mapContainer">
+          ...put Map Component here...
       </div>
-      <div className="quakeContainer">
-        <h1>Earthquakes from the past week:</h1>
+        <div className="quakeContainer">
+          <h1>Earthquakes from the past week:</h1>
         ...put Quakes Component here...
       </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
