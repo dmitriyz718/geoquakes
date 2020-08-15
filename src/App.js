e08e4a9e4b756d7eb9002a234133f44f1e7cf468
u101004132 <dzag17188@gmail.com>
2020-08-15 16:18:22 -0400

    set initial state with an empty Earthquakes array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    Earthquakes: [],
+  }
   render() {
     return (
       <div className="app" >
