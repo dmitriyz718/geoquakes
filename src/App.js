c13d568b7b9bb28894a3b672b1cbf3c4d86b305c
u101004132 <dzag17188@gmail.com>
2020-08-15 17:25:55 -0400

    got quakes and state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import './App.css';
 class App extends Component {
   state = {
     Earthquakes: [],
-  }
+  };
+
+  componentDidMount() {
+    const getQuakes = async () => {
+      let response = await (await fetch(`https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson`)).json();;
+      this.setState({ Earthquakes: response.features });
+    }
+    getQuakes();
+  };
+
   render() {
     return (
       <div className="app" >
